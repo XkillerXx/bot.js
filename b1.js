4ac2aaa0db2799abe3c74f687721f88c33013618
XkillerXx <38698562+XkillerXx@users.noreply.github.com>
2018-07-21 17:16:11 +0300

    Update b1.js

--- b1.js
+++ b1.js
@@ -170,7 +170,7 @@ msg.delete();
 
 
 client.on('message', message => {
-    const prefix = '$'
+    
 var args = message.content.split(" ").slice(1);    
 if(message.content.startsWith(prefix + 'id')) {
 var year = message.author.createdAt.getFullYear()
