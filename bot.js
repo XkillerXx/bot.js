7402ce444e8cfe20d05221a46f0ec9a861af573e
XkillerXx <38698562+XkillerXx@users.noreply.github.com>
2019-03-25 20:33:57 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -349,25 +349,54 @@ client.on('message', message => {
 });
  
 var prefix = "$";
-
- 
 client.on('message', message => {
-    if (!message.guild) return;
-    if (message.content.startsWith(prefix + "رابط")) {
+
+    if (message.content.startsWith("رابط")) {
 
         message.channel.createInvite({
-        thing: true,
-        maxUses: 5,
-        maxAge: 86400
-    }).then(invite =>
-      message.author.sendMessage(invite.url)
-    )
-  message.channel.send(`** تم أرسال الرابط برسالة خاصة **`)
 
-      message.author.send(`**مدة الرابط : يـوم
- عدد استخدامات الرابط : 5 **`)
+        thing: true,
+
+        maxUses: 5,
+
+        maxAge: 86400,
+
+    }).then(invite =>
+
+      message.author.sendMessage(invite.url)
+
+    )
+
+    const embed = new Discord.RichEmbed()
+
+        .setColor("RANDOM")
+
+          .setDescription("تم أرسال الرابط برسالة خاصة")
+
+           .setAuthor(client.user.username, client.user.avatarURL)
+
+                 .setAuthor(client.user.username, client.user.avatarURL)
+
+                .setFooter('طلب بواسطة: ' + message.author.tag)
+
+      message.channel.sendEmbed(embed).then(message => {message.delete(10000)})
+
+              const Embed11 = new Discord.RichEmbed()
+
+        .setColor("RANDOM")
+
+        
+
+    .setDescription("** مدة الرابط : يوم | عدد استخدامات الرابط : 5 **")
+
+      message.author.sendEmbed(Embed11)
+
     }
-});
+
+}); 
+
+ 
+        
 
 
 client.on('message', message => {
